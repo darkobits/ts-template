c195a154ecf0545e13ba3405f494228edc912b52
darkobits <joshua+github@frontlawn.net>
2020-12-05 17:40:15 -0800

    ⍺

---
+++ README.md
@@ -0,0 +1,19 @@
+This is a [template repository][template-repository-url] that can be used to
+quickly bootstrap new Node libraries.
+
+The minimal (but robust) toolchain consists of [`tsdown`][tsdown],
+[`eslint`][eslint], and [`vitest`][vitest].
+
+Click **Use this template** at the top-right of this page to clone this
+repository, or:
+
+<p align="center">
+  <a href="https://codesandbox.io/p/github/darkobits/ts-template/codesandbox?embed=1&file=%2Fsrc%2Findex.ts&showConsole=true">
+    <img width="160" alt="Edit on Codesandbox" src="https://github.com/user-attachments/assets/fa81ea3b-3bd6-421c-85fd-772c1dee77a8">
+  </a>
+</p>
+
+[tsdown]: https://tsdown.dev
+[eslint]: https://eslint.org
+[vitest]: https://vitest.dev
+[template-repository-url]: https://docs.github.com/en/repositories/creating-and-managing-repositories/creating-a-repository-from-a-template?search-overlay-input=template+repository#about-repository-templates
---
+++ tsdown.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from 'tsdown/config'
+
+export default defineConfig({
+  /**
+   * Any TypeScript file (except for tests) in the source directory should be
+   * considered an entry.
+   *
+   * @see https://tsdown.dev/reference/config-options#entry
+   */
+  entry: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/*.{spec,test}.{ts,tsx,js,jsx}'],
+  /**
+   * Do not bundle output into a single file. Instead, use the same directory
+   * structure as source files.
+   *
+   * @see https://tsdown.dev/reference/config-options#unbundle
+  */
+  unbundle: true,
+  /**
+  * Check for unused dependencies with `unplugin-unused`.
+  *
+  * @see https://tsdown.dev/reference/config-options#unused
+  */
+  unused: true,
+  /**
+  * After building, run `publint`.
+  *
+  * @see https://tsdown.dev/reference/config-options#publint
+  */
+  publint: true,
+  /**
+   * Emit TypeScript declaration files (.d.ts).
+   */
+  dts: true,
+  /**
+   * Automatically keeps the `main`, `module`, `types`, and `exports` fields in
+   * package.json in sync with the correct files in the output directory.
+   *
+   * @see https://tsdown.dev/reference/config-options#exports
+   */
+  exports: true
+})
