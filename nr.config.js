bab25c8df1edb04b45ac9a53301c60284bdcbe43
darkobits <joshua+github@frontlawn.net>
2020-12-05 17:40:15 -0800

    ⍺

---
+++ nr.config.js
@@ -0,0 +1,9 @@
+import { nr } from '@darkobits/ts';
+
+
+/**
+ * See: https://github.com/darkobits/nr
+ */
+export default nr(({ command, task, script }) => {
+  // Add your own commands, tasks, and scripts here.
+});
